45213827d821340db38eef4795ad1b0ef3b1736d
MrDeath22 <daller3017@gmail.com>
2021-10-08 07:38:28 +0200

    pricing section added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 let elSiteNav = document.querySelector('.site-nav');
 let elOpenBtn = document.querySelector('.site-menu__btn');
+let elBody = document.querySelector("body")
 
 elOpenBtn.addEventListener('click', () => {
   elSiteNav.classList.toggle('show');
 })
+
+let darkLightbtn = document.querySelector('.dark-light-btn');
+darkLightbtn.addEventListener('click', function() {
+  elBody.classList.toggle('dark-content');
+})
+
+
+
